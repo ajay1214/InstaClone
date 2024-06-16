1abe49a8b1ea6ead82972640b74f9c5270e8e863
Ajay Bind <adarshbind61@gmail.com>
2024-06-16 18:01:36 +0530

    security rules in firebase updated

--- src/components/AuthForm/GoogleAuth.jsx
+++ src/components/AuthForm/GoogleAuth.jsx
@@ -6,42 +6,45 @@ import useAuthStore from "../../store/authStore";
 import { doc, getDoc, setDoc } from "firebase/firestore";
 
 const GoogleAuth = ({ prefix }) => {
-	const [signInWithGoogle, , , error] = useSignInWithGoogle(auth);
+	const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
 	const showToast = useShowToast();
 	const loginUser = useAuthStore((state) => state.login);
 
 	const handleGoogleAuth = async () => {
 		try {
-			const newUser = await signInWithGoogle();
-			if (!newUser && error) {
-				showToast("Error", error.message, "error");
-				return;
-			}
-			const userRef = doc(firestore, "users", newUser.user.uid);
-			const userSnap = await getDoc(userRef);
+			const result = await signInWithGoogle();
+			if (result && result.user) {
+				const user = result.user;
+				const userRef = doc(firestore, "users", user.uid);
+				const userSnap = await getDoc(userRef);
 
-			if (userSnap.exists()) {
-				// login
-				const userDoc = userSnap.data();
-				localStorage.setItem("user-info", JSON.stringify(userDoc));
-				loginUser(userDoc);
+				if (userSnap.exists()) {
+					// login
+					const userDoc = userSnap.data();
+					localStorage.setItem("user-info", JSON.stringify(userDoc));
+					loginUser(userDoc);
+				} else {
+					// signup
+					const userDoc = {
+						uid: user.uid,
+						email: user.email,
+						username: user.email.split("@")[0],
+						fullName: user.displayName,
+						bio: "",
+						profilePicURL: user.photoURL,
+						followers: [],
+						following: [],
+						posts: [],
+						createdAt: Date.now(),
+					};
+					await setDoc(doc(firestore, "users", user.uid), userDoc);
+					localStorage.setItem("user-info", JSON.stringify(userDoc));
+					loginUser(userDoc);
+				}
 			} else {
-				// signup
-				const userDoc = {
-					uid: newUser.user.uid,
-					email: newUser.user.email,
-					username: newUser.user.email.split("@")[0],
-					fullName: newUser.user.displayName,
-					bio: "",
-					profilePicURL: newUser.user.photoURL,
-					followers: [],
-					following: [],
-					posts: [],
-					createdAt: Date.now(),
-				};
-				await setDoc(doc(firestore, "users", newUser.user.uid), userDoc);
-				localStorage.setItem("user-info", JSON.stringify(userDoc));
-				loginUser(userDoc);
+				if (error) {
+					showToast("Error", error.message, "error");
+				}
 			}
 		} catch (error) {
 			showToast("Error", error.message, "error");
